deb9b226713374d375a9c8c15361b2d02643ad8b
Sean Seefried <sean.seefried@gmail.com>
2013-05-21 16:07:10 +1000

    Everything expressed in standard Cartesian co-ordinates now.
    This means x axis goes right, y axis goes up and angles
    are measured anti-clockwise from x-axis.

--- game.js
+++ game.js
@@ -84,9 +84,13 @@ var Lorentz = function(c) {
   var scale = function(s,v) {
     return { x: s*v.x, y: s*v.y};
   };
+  
+  var invScale = function(s,v) {
+    return { x: v.x/s, y: v.y/s};
+  }
 
   var angle = function(v) {
-    return Math.atan2(v.x, v.y);
+    return Math.atan2(v.y, v.x);
   };
 
   var beta = function(v) {
@@ -109,6 +113,7 @@ var Lorentz = function(c) {
     sub: sub,
     mag: mag,
     scale: scale,
+    invScale: invScale,
     angle: angle,
     beta: beta,
     boost: boost
@@ -143,7 +148,7 @@ var game = (function () {
     //
     // The time rate measures how many seconds pass in S as opposed to P.
     //
-    ship    = { pos: { x:0, y:0 }, angle: 0.0, v: { x: 0, y: 0 }, timeRate: 1.0 };
+    ship    = { pos: { x:0, y:0 }, angle: 0, v: { x: 0, y: 0 }, timeRate: 1.0 };
     planets = randomPlanets(30);
     trans   = translate(0,0, width, height,1);
 
@@ -181,8 +186,8 @@ var game = (function () {
   var drawShip = function(thruster) {
     var pos = ship.pos,
         len = width/100, i, p,
-        points = [ {pos: {x: pos.x,     y: pos.y-len }},
-                   {pos: {x: pos.x-len, y: pos.y+len }},
+        points = [ {pos: {x: pos.x-len, y: pos.y     }},
+                   {pos: {x: pos.x+len, y: pos.y-len }},
                    {pos: {x: pos.x+len, y: pos.y+len }} ],
         path = "";
         s = trans(ship);
@@ -196,10 +201,10 @@ var game = (function () {
     path = "M"+p.pos.cx+","+p.pos.cy+path+"L"+p.pos.cx+","+p.pos.cy;
 
     if (thruster) {
-      st.push(r.rect(ship.pos.cx-len/4, ship.pos.cy, len/2, len*2).attr({fill: "orange"}));
+      st.push(r.rect(ship.pos.cx-2*len, ship.pos.cy-len/4, len*2, len/2).attr({fill: "orange"}));
     }
     st.push(r.path(path).attr({fill: "teal"}));
-    st.rotate(Raphael.deg(ship.angle),s.pos.cx,s.pos.cy);
+    st.rotate(Raphael.deg(-ship.angle),s.pos.cx,s.pos.cy);
   }
 
   var drawDirectionVector = function(v) {
@@ -210,11 +215,12 @@ var game = (function () {
     var dl = 25;
     var dcx = width - (dl+paddingX), dcy = (dl+paddingY);
     var lineLen = (speed/c)*dl; // proportional to the speed
-    var dx = lineLen*Math.sin(angle),dy = lineLen*Math.cos(angle); // Direction x and y for direction vector draw in top right.
+    var dx = lineLen*Math.cos(angle),
+        dy = lineLen*Math.sin(angle); // Direction x and y for direction vector draw in top right.
     var dcolor = "pink";
 
     r.circle(dcx,dcy,dl).attr({stroke: dcolor, "stroke-width": "2"});
-    r.path("M" + dcx + "," + dcy + "L" + (dcx-dx) + "," + (dcy + dy)).attr({stroke: dcolor, "stroke-width": "2px"});
+    r.path("M" + dcx + "," + dcy + "L" + (dcx+dx) + "," + (dcy - dy)).attr({stroke: dcolor, "stroke-width": "2px"});
   }
 
   /*
@@ -241,29 +247,54 @@ var game = (function () {
 
   var drawPlanets = function(v) {
     var tx, ty, i, ang = lz.angle(v), scaleFactor = lz.beta(v),
-        middlePath = "r"+Raphael.deg(ang)+ "s1,"+scaleFactor+ "r"+(-Raphael.deg(ang));
+        middlePath = "r"+Raphael.deg(Math.PI/2-ang)+ "s1,"+scaleFactor+ "r"+(-Raphael.deg(Math.PI/2 - ang));
 
-    trans(ship); // important. Need to have correct .pos.cx and .pos.cy for drawing planets.
+    trans(ship); // Important. Need to have correct .pos.cx and .pos.cy for drawing planets.
     for (i=0; i < planets.length; i++) {
       p = trans(planets[i]);
-      tx = ship.pos.cx - p.pos.cx;
+      tx = ship.pos.cx + p.pos.cx;
       ty = ship.pos.cy - p.pos.cy;
       path = "t"+tx+","+ty+ middlePath + "t"+(-tx)+","+(-ty);
-      r.circle(p.pos.cx, p.pos.cy, p.cr).attr({ fill: "grey"}).transform(path);
+      r.circle(-p.pos.cx, -p.pos.cy, p.cr).attr({ fill: "grey"}).transform(path);
     }
-
-  }
-
-  var thrust = function() {
-    var deltaV = { x: accel*Math.sin(ship.angle), y: accel*Math.cos(ship.angle) };
-    ship.v = lz.scale(1/(1 - lz.dot(ship.v, deltaV)/(c*c)),lz.sub(ship.v, deltaV));
-
-    // FIXME: Still need this fudge factor! Annoying! Floating point maths 
-    // is the problem I think.
-    ship.v = lz.scale(0.999, ship.v);
 
   };
 
+
+  var g;
+
+  var thrust = function() {
+
+    ship.v = lz.add(ship.v,{ x: accel*Math.cos(ship.angle), y: accel*Math.sin(ship.angle)});
+    console.log(ship.v);
+    console.log(lz.angle(ship.v));
+
+  //   if (g === undefined) {
+  //   var  sa = lz.angle(ship.v),
+  //        ang = sa - ship.angle, // angle of the velocity relative to velocity of ship
+  //         v = { x: ship.v.x*Math.cos(sa), y: ship.v.y*Math.sin(sa)},
+  //         u = { x: accel*Math.sin(ang), y: accel*Math.cos(ang) },
+  //         gamma = 1/(1 + (u.x*v.x)/(c*c)),
+  //         b     = 1/lz.beta(v);
+  //         // log("v")(v);
+  //         // log("gamma")(gamma);
+  //         // log("b")(b);
+  // 
+  //         log("v")(v);
+  //         log("u")(u);
+  //         log("sa")(sa);
+  //         log("ang")(ang);
+  // 
+  // 
+  // var newV = { x: u.x*gamma*b, y: (u.y + v.y)*gamma };
+  // log("newV")(newV);
+  // ship.v = newV;
+  // log("ship.v")(ship.v);
+  // log("----------")('----------');
+  // }
+//  g = 1;
+  };
+  
   var animate = function() {
     var maxPercent = 0.999;
     var angleInc = 0.12;
@@ -271,7 +302,7 @@ var game = (function () {
     var i,p;
     var speed = lz.mag(ship.v);
     var path;
-    var percentOfC = Math.round(speed/c*1000)/10;
+    var percentOfC = Math.round(speed/c*100000000)/1000000;
 
     $('#stats').text(percentOfC + "% c");
 
@@ -281,11 +312,11 @@ var game = (function () {
 
     // Update ship state
     if (keyboard.keydown(37)) {
-      ship.angle -= angleInc;
+      ship.angle += angleInc;
     }
 
     if (keyboard.keydown(39)) {
-      ship.angle += angleInc;
+      ship.angle -= angleInc;
     }
 
     // Thruuuuust!
